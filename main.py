26ee4c0c50495fa261a1784c2aec68ed463afca1
ぺろぱ <53344626+Syulis@users.noreply.github.com>
2020-10-13 23:05:35 +0900

    デスクトップ版LINEVer.6.3.2.2338 で動作確認
    LINEを起動し、動作させるだけです

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import pyautogui
+
+loop = 0
+
+while loop < 10000:
+    time.sleep(1)
+    pyautogui.click(pyautogui.locateCenterOnScreen("source/01.png"))
+    time.sleep(1)
+    pyautogui.click(pyautogui.locateCenterOnScreen("source/02.png"))
+    time.sleep(1)
+    pyautogui.click(pyautogui.locateCenterOnScreen("source/03.png"))
+    time.sleep(62)
+    pyautogui.click(pyautogui.locateCenterOnScreen("source/04.png"))
+    loop += 1
